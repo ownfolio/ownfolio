663ff166d7d34d9b7aac183289546632477dd6e8
Christian Hoffmeister <mail@choffmeister.de>
2024-09-23 16:56:32 +0200

    fix: adjust to new yahoo finance api

--- src/server/quotes/yahooFinance.ts
+++ src/server/quotes/yahooFinance.ts
@@ -1,16 +1,22 @@
 import axios from 'axios'
 import BigNumber from 'bignumber.js'
-import { parse } from 'csv-parse/sync'
 
-type YahooFinanceCsvFormat = {
-  Date: string
-  Open: string
-  High: string
-  Low: string
-  Close: string
-  'Adj Close': string
-  Volume: string
-}[]
+type YahooFinanceJsonFormat = {
+  chart: {
+    result: {
+      timestamp: number[]
+      indicators: {
+        quote: {
+          open: number[]
+          close: number[]
+          high: number[]
+          low: number[]
+        }[]
+      }
+    }[]
+    error: unknown | null
+  }
+}
 
 export type YahooFinanceDateResult = { open?: BigNumber; high?: BigNumber; low?: BigNumber; close: BigNumber }
 export type YahooFinanceResult = { [date: string]: YahooFinanceDateResult | undefined }
@@ -20,7 +26,7 @@ export const yahooFinanceAxios = axios.create()
 export async function fetchYahooFinanceQuotes(symbol: string): Promise<YahooFinanceResult> {
   const baseURL = 'https://query1.finance.yahoo.com'
 
-  const resCsv = await yahooFinanceAxios.get<string>(`/v7/finance/download/${symbol}`, {
+  const resJson = await yahooFinanceAxios.get<YahooFinanceJsonFormat>(`/v8/finance/chart/${symbol}`, {
     baseURL: baseURL,
     params: {
       period1: 0,
@@ -29,17 +35,24 @@ export async function fetchYahooFinanceQuotes(symbol: string): Promise<YahooFina
       events: 'history',
     },
   })
-  const resCsvResult = (await parse(resCsv.data, { delimiter: ',', columns: true })) as YahooFinanceCsvFormat
 
   const result: YahooFinanceResult = {}
-  resCsvResult.forEach(elem => {
-    if (elem.Date !== 'null' && elem.Close !== 'null') {
-      result[elem.Date] = {
-        open: elem.Open !== 'null' ? BigNumber(elem.Open) : undefined,
-        high: elem.Open !== 'null' ? BigNumber(elem.High) : undefined,
-        low: elem.Open !== 'null' ? BigNumber(elem.Low) : undefined,
-        close: BigNumber(elem.Close),
-      }
+  resJson.data.chart.result[0].timestamp.forEach((ts, idx) => {
+    const date = new Date(ts * 1000).toISOString().substring(0, 10)
+    result[date] = {
+      open: BigNumber(
+        resJson.data.chart.result[0].indicators.quote[0].open[idx] ||
+          resJson.data.chart.result[0].indicators.quote[0].close[idx]
+      ),
+      high: BigNumber(
+        resJson.data.chart.result[0].indicators.quote[0].high[idx] ||
+          resJson.data.chart.result[0].indicators.quote[0].close[idx]
+      ),
+      low: BigNumber(
+        resJson.data.chart.result[0].indicators.quote[0].low[idx] ||
+          resJson.data.chart.result[0].indicators.quote[0].close[idx]
+      ),
+      close: BigNumber(resJson.data.chart.result[0].indicators.quote[0].close[idx]),
     }
   })
   return result
